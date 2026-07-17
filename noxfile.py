e7f757e1ce17720eb69bf69bfb4b1ff495cc5c53
Sarah R Moeller <research.lgx@gmail.com>
2026-07-17 15:20:57 +0000

    removed empty lines

--- noxfile.py
+++ noxfile.py
@@ -28,4 +28,3 @@ def docs(session: nox.Session) -> None:
     if args.serve:
         session.log("Launching docs at http://localhost:8000/ - use Ctrl-C to quit")
         session.run("python", "-m", "http.server", "8000", "-d", "_build/html")
-
